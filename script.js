e2db6dd9b7acf31b2bf4fc68433d1d7ad6dbafba
Kshyap Thanki <kt.dctcedu@live.com>
2021-09-10 13:49:37 -0500

    adds comments in JS

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const shareBtn = document.querySelector(".share-btn");
 
 const shareLinks = document.querySelector(".share-links-container ");
 
+///Toggle Share Button
 shareBtn.addEventListener("click", function () {
   shareLinks.classList.toggle("hidden");
 });
+
+/// Press ESC to Close Share link container
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     shareLinks.classList.add("hidden");
